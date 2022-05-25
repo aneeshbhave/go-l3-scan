c4b20fe310dcbbbbcd55fb80cf04256ae828b5e5
aneeshb <aneesh1701@gmail.com>
2022-05-25 15:39:00 +0530

    removed extra files

--- fio.go
+++ fio.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"bufio"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
+
+func dir_ls_rec(dir_path string) []string {
+	w := Walker{}
+	filepath.WalkDir(dir_path, w.walk)
+	return w.paths
+}
+
+type Walker struct {
+	paths []string
+}
+
+func (w *Walker) walk(s string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if !d.IsDir() {
+		w.paths = append(w.paths, s)
+	}
+	return nil
+}
 
 func read_file_str(fpath string) string {
 	data, err := ioutil.ReadFile(fpath)
--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
-	"path/filepath"
 )
 
-type Walker struct {
-	paths []string
-}
-
 func main() {
-	w := Walker{}
-	filepath.WalkDir("/home/aneesh", w.walk)
-	fmt.Println(w.paths)
-}
-
-func (w *Walker) walk(s string, d fs.DirEntry, err error) error {
-	if err != nil {
-		return err
-	}
-	if !d.IsDir() {
-		w.paths = append(w.paths, s)
-	}
-	return nil
+	fmt.Println(dir_ls_rec("/home/aneesh/Code"))
 }
